8c0a00b88c659071d1bac9082eb9107fe142950c
pierrehuitieme <adrian.siwiec@gmail.com>
2016-01-20 17:41:51 +0100

    Better colors

--- render.sh
+++ render.sh
@@ -3,13 +3,18 @@ function render
 	screen=""		#moved screen from disk to RAM - faster rendering
 	#echo >screen #faster PC MASTER RACE - ONLY 144 FPS, and no flickering
 	tmpChar=' '
+	background="\e[48;5;119m"
+	snake_color="\e[38;5;208m"
+	normal="\e[0m"
+	apple_color="\e[38;5;9m"
+	wall_color="\e[48;5;17m"
 	for ((i=0; i<height[$lvl]; i++)) do
 		for ((j=0; j<width[$lvl]; j++)) do
 			case ${matrix[$i,$j]} in
-				0) tmpChar='\e[40m \e[0m' ;; #nothing
-			 	1) tmpChar='\e[47mX\e[0m' ;;
-				2) tmpChar='O' ;;
-				4) tmpChar='\e[31m@\e[0m' ;;
+				0) tmpChar="$background\e[1m $normal" ;; #nothing
+			 	1) tmpChar="$wall_color\e[1m $normal" ;;
+				2) tmpChar="$snake_color$background\e[1mO$normal" ;;
+				4) tmpChar="$background$apple_color\e[1m@$normal" ;;
 			esac
 			screen+="$tmpChar"
 		done
@@ -26,5 +31,5 @@ function render
 		screen+="\n"
 	done
 	clear		# double buffering lol
-	echo -e $screen
+	echo -e "$screen"
 }
